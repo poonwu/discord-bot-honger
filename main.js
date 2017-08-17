b23051e5885c423fd76af2f7da2b67fec35bc379
Poon <poon.wuthi@gmail.com>
2017-08-17 22:28:05 +0700

    Added basic polling function

--- main.js
+++ main.js
@@ -1,1 +1,70 @@
 const Discord = require('discord.js');
+const AsyncPolling = require('async-polling');
+const cheerio = require('cheerio');
+const axios = require('axios');
+const config = require('./config.json');
+
+var $ = null;
+var novelLink = config.novel_url;
+var chapterLink = null;
+var chapterTitle = null;
+var polling = AsyncPolling(function(end) {
+    axios.get(novelLink).then((res) => {
+        $ = cheerio.load(res.data);
+        let newChapterTitle = $('a.chap').clone().children().remove().end().text().trim();
+        let newChapterLink = $('a.chap').attr('href');
+
+        if(newChapterLink != chapterLink) {
+            chapterLink = newChapterLink;
+            chapterTitle = newChapterTitle;
+
+            broadcast('Ohhhh. master @here, new chapter is out!!\n Chapter name is uhh... oh...' + chapterTitle + '\n');
+        }
+        end();
+    })
+    .catch((err) => {
+        console.error(err);
+        end();
+    });
+}, 5000);
+
+function broadcast(content) {
+    client.guilds.array().forEach(g => {
+        let channel = g.channels.find('name', 'general');
+        channel.send(content);
+    });
+}
+
+client = new Discord.Client();
+    
+client.on('ready', () => {
+    console.log('Bot is ready');
+    console.log(`https://discordapp.com/api/oauth2/authorize?client_id=${config.client_id}&scope=bot&permissions=${config.permissions}`);
+
+    // get initial chapter link
+    axios.get(novelLink)
+    .then((res) => {
+        $ = cheerio.load(res.data);
+        chapterTitle = $('a.chap').clone().children().remove().end().text().trim().split('：')[1];
+        chapterLink = $('a.chap').attr('href');
+
+        polling.run();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+});
+
+client.on('message', message => {
+    // only if Hong'er is mentioned
+    if(!message.mentions.everyone && message.mentions.users.find('id', client.user.id)) {
+        message.channel.send('zzzZZZZ');
+    }
+
+    if(message.content === '!check') {
+        broadcast(chapterTitle);
+    }
+});
+
+
+client.login(config.token);
